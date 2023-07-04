2f8177dacd8987a0a3233e5434a8809e6c2346ac
trungkien <trungkien.nguyen@tui.co.uk>
2023-07-04 21:15:54 +0700

    push code

--- Assets/AutomatedQA/GeneratedTests/PageObjects/Scene_MenuScene_PageObject.cs
+++
@@ -1,9 +0,0 @@
-namespace GeneratedAutomationTests
-{
-    public class Scene_MenuScene_PageObject
-    {
-        public static string SceneName = "MenuScene";
-
-        public static string Clickable_MenuScene_PlayButton = "Canvas/MainMenu/PlayButton"; 
-    }
-}
